716ee34ca39659a0f61ad41baf123c21d813709e
casatoo <casato6666@gmail.com>
2023-04-18 23:37:45 +0900

    아이템 5종 추가, 아이템 레벨업 시스템 도입, 아이템 UI 추가

--- Assets/Undead Survivor/Code/Weapon.cs
+++ Assets/Undead Survivor/Code/Weapon.cs
@@ -14,10 +14,7 @@ public class Weapon : MonoBehaviour
     Player player;
 
     void Awake() {
-        player = GetComponentInParent<Player>();
-    }
-    void Start() {
-        Init();
+        player = GameManager.instance.player;
     }
 
     void Update() {
@@ -47,17 +44,36 @@ public class Weapon : MonoBehaviour
 
         if(id == 0)
             Batch();
+
+        player.BroadcastMessage("ApplyGear",SendMessageOptions.DontRequireReceiver);
     }
-    public void Init() {
+    public void Init(ItemData data) {
+        //basic set
+        name = "Weapon" + data.itemId;
+        transform.parent = player.transform;
+        transform.localPosition = Vector3.zero;
+        //property set
+        id = data.itemId;
+        damage = data.baseDamage;
+        count = data.baseCount;
+
+        for(int index=0;index < GameManager.instance.pool.prefabs.Length; index++){
+            if(data.projectile == GameManager.instance.pool.prefabs[index]){
+                prefabId = index;
+                break;
+
+            }
+        }
         switch (id) {
             case 0:
                 speed = 150;
                 Batch();
                 break;
             default:
-                speed = 0.3f;
+                speed = 0.4f;
                 break;
         }
+        player.BroadcastMessage("ApplyGear", SendMessageOptions.DontRequireReceiver);
     }
     void Batch() {
         for(int index = 0; index < count; index++) {
